f6064f3c296e54c98126fe43b1149947f6830ce4
richy0101 <richy0101@gmail.com>
2014-12-09 13:39:10 -0800

    Initial Commit

---
+++ Official_KPMG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Official_KPMG
+//
+//  Created by Richard Phillips on 12/9/14.
+//  Copyright (c) 2014 Richard Phillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
